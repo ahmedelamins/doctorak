e5768a3a10b710813fb90c3a892f177e32006954
eddyiiz <eddyxsayf@gmail.com>
2024-10-06 20:55:53 +0200

    add user model

--- Doctorak.Server/Models/User.cs
+++ Doctorak.Server/Models/User.cs
@@ -1,10 +1,15 @@
 ﻿namespace Doctorak.Server.Models;
 public class User
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+    [Required]
     public string Email { get; set; } = string.Empty;
+    [Required]
     public string FirstName { get; set; } = string.Empty;
+    [Required]
     public string LastName { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-    public string ConfirmPassword { get; set; } = string.Empty;
+    public byte[] PasswordHash { get; set; } = new byte[32];
+    public byte[] PasswordSalt { get; set; } = new byte[32];
 }
