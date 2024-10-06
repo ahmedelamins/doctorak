3442f52f7fcf0cb69ed8f4a8c5ea2a7a0dea8358
eddyiiz <eddyxsayf@gmail.com>
2024-10-06 20:59:23 +0200

    add service response model

---
+++ Doctorak.Server/Models/ServiceResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Doctorak.Server.Models;
+public class ServiceResponse<T>
+{
+    public T Data { get; set; }
+    public string Message { get; set; } = "Successful";
+    public bool Success { get; set; } = true;
+}
