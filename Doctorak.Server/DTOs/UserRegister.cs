95536532f3b128c845ea101cf2c97075ee1d718c
eddyiiz <eddyxsayf@gmail.com>
2024-10-07 09:15:53 +0200

    add validation

--- Doctorak.Server/DTOs/UserRegister.cs
+++ Doctorak.Server/DTOs/UserRegister.cs
@@ -3,7 +3,6 @@ public class UserRegister
 {
     [EmailAddress]
     public string? Email { get; set; }
-    [Phone]
     public string? Number { get; set; }
     [Required]
     public string FirstName { get; set; } = string.Empty;
@@ -13,4 +12,18 @@ public class UserRegister
     public string Password { get; set; } = string.Empty;
     [Required, Compare("Password", ErrorMessage = "passwords do not match")]
     public string ConfirmPassword { get; set; } = string.Empty;
+
+    //validation to ensure either phone number or email is provided
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (string.IsNullOrEmpty(Email) && string.IsNullOrEmpty(Number))
+        {
+            yield return new ValidationResult("Either Email or Phone number must be provided.");
+        }
+        // Check if the phone number is valid if provided
+        if (!string.IsNullOrEmpty(Number) && !System.Text.RegularExpressions.Regex.IsMatch(Number, @"^\d+$"))
+        {
+            yield return new ValidationResult("Invalid phone number format. Only digits are allowed.");
+        }
+    }
 }
