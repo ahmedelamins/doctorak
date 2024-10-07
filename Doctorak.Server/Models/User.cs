4c72e0bde347f6d51e8c68b2a54c025ff2c330aa
eddyiiz <eddyxsayf@gmail.com>
2024-10-07 13:44:29 +0200

    update model

--- Doctorak.Server/Models/User.cs
+++ Doctorak.Server/Models/User.cs
@@ -1,33 +1,20 @@
 ﻿namespace Doctorak.Server.Models;
+
 public class User
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-
-    //first and last name for clarity 
+    [EmailAddress]
+    [Required]
+    public string Email { get; set; } = string.Empty;
     [Required]
     public string FirstName { get; set; } = string.Empty;
     [Required]
     public string LastName { get; set; } = string.Empty;
-
-    // register with an email OR a phone number
-    [EmailAddress]
-    public string? Email { get; set; }
-    [Phone]
-    public string? Number { get; set; }
-
-    //trcack email/number confirmation
     public bool IsEmailConfirmed { get; set; } = false;
-    public bool IsNumberConfirmed { get; set; } = false;
-
-    //store code/token for verifications
     public string? EmailConfirmationToken { get; set; }
-    public string? NumberConfirmationToken { get; set; }
-
-    //password hash and salt
     public byte[] PasswordHash { get; set; } = new byte[32];
     public byte[] PasswordSalt { get; set; } = new byte[32];
-
     public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
