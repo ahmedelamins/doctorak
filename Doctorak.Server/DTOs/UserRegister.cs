311deedf4cccb4c83e5d70cdd36c761b6fde129e
eddyiiz <eddyxsayf@gmail.com>
2024-10-07 08:04:31 +0200

    Add UserRegister DTO

---
+++ Doctorak.Server/DTOs/UserRegister.cs
@@ -0,0 +1,16 @@
+﻿namespace Doctorak.Server.DTOs;
+public class UserRegister
+{
+    [EmailAddress]
+    public string? Email { get; set; }
+    [Phone]
+    public string? Number { get; set; }
+    [Required]
+    public string FirstName { get; set; } = string.Empty;
+    [Required]
+    public string LastName { get; set; } = string.Empty;
+    [Required]
+    public string Password { get; set; } = string.Empty;
+    [Required, Compare("Password", ErrorMessage = "passwords do not match")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
