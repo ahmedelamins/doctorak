736622b417e636f6b66aee6b47c67b7477b1160c
eddyiiz <eddyxsayf@gmail.com>
2024-10-07 07:31:36 +0200

    add user exists method

--- Doctorak.Server/Services/AuthService/AuthService.cs
+++ Doctorak.Server/Services/AuthService/AuthService.cs
@@ -1,4 +1,36 @@
 ﻿namespace Doctorak.Server.Services.AuthService;
 public class AuthService : IAuthService
 {
+    private readonly DataContext _context;
+
+    public AuthService(DataContext context)
+    {
+        _context = context;
+    }
+    public async Task<ServiceResponse<int>> Register(User user, string password)
+    {
+        var response = new ServiceResponse<int>();
+
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
+    }
+
+    //check if a user exists
+    public async Task<bool> UserExists(string email, string number)
+    {
+        return await _context.Users.AnyAsync(u =>
+            (u.Email != null && u.Email.ToLower().Equals(email.ToLower())) ||
+            (u.Number != null && u.Number.Equals(number))
+        );
+    }
+
 }
