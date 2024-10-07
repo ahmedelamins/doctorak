5fc7aa645fa773a9091d63093bc0b6472e142d7f
eddyiiz <eddyxsayf@gmail.com>
2024-10-07 07:06:17 +0200

    add additional properties

--- Doctorak.Server/Models/User.cs
+++ Doctorak.Server/Models/User.cs
@@ -4,12 +4,27 @@ public class User
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    [Required]
-    public string Email { get; set; } = string.Empty;
+
+    //first and last name for clarity 
     [Required]
     public string FirstName { get; set; } = string.Empty;
     [Required]
     public string LastName { get; set; } = string.Empty;
+
+    // register with an email OR a phone number
+    [EmailAddress]
+    public string? Email { get; set; } = string.Empty;
+    public string? Number { get; set; } = string.Empty;
+
+    //trcack email/number confirmation
+    public bool IsEmailConfirmed { get; set; } = false;
+    public bool IsNumberConfirmed { get; set; } = false;
+
+    //store code/token for verifications
+    public string? EmailConfirmationToken { get; set; }
+    public string? NumberConfirmationToken { get; set; }
+
+    //password hash and salt
     public byte[] PasswordHash { get; set; } = new byte[32];
     public byte[] PasswordSalt { get; set; } = new byte[32];
 }
