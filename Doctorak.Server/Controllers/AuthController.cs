1b4371f9af8e618f83252b0bc177b211d101ae01
eddyiiz <eddyxsayf@gmail.com>
2024-10-08 14:32:52 +0200

    add confirm email route

--- Doctorak.Server/Controllers/AuthController.cs
+++ Doctorak.Server/Controllers/AuthController.cs
@@ -1,44 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Doctorak.Server.Controllers
+namespace Doctorak.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthController : ControllerBase
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
     {
-        private readonly IAuthService _authService;
+        _authService = authService;
+    }
 
-        public AuthController(IAuthService authService)
+    [HttpPost("register")]
+    public async Task<ActionResult<ServiceResponse<int>>> Register([FromBody] UserRegister request)
+    {
+        if (string.IsNullOrEmpty(request.Email))
         {
-            _authService = authService;
+            return BadRequest("an Email must be provided.");
         }
 
-        [HttpPost("register")]
-        public async Task<ActionResult<ServiceResponse<int>>> Register([FromBody] UserRegister request)
+        var response = await _authService
+            .Register(
+                new User
+                {
+                    Email = request.Email,
+                    FirstName = request.FirstName,
+                    LastName = request.LastName,
+                },
+                request.Password
+            );
+
+        if (!response.Success)
         {
-            if (string.IsNullOrEmpty(request.Email))
-            {
-                return BadRequest("an Email must be provided.");
-            }
-
-            var response = await _authService
-                .Register(
-                    new User
-                    {
-                        Email = request.Email,
-                        FirstName = request.FirstName,
-                        LastName = request.LastName,
-                    },
-                    request.Password
-                );
-
-            if (!response.Success)
-            {
-                return BadRequest(response);
-            }
-
-
-            return Ok(response);
+            return BadRequest(response.Message);
         }
+
+
+        return Ok(response);
+    }
+
+    [HttpGet("confirm-email")]
+    public async Task<ActionResult<ServiceResponse<bool>>> ConfirmEmail(string token, string email)
+    {
+        var response = await _authService.ConfirmEmail(token, email);
+
+        if (!response.Success)
+        {
+            return BadRequest(response.Message);
+        }
+
+        return Ok(response);
     }
 }
