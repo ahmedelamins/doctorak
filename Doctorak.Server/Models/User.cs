6e4aba227e618b239353db1c4f745cdd4bb47a90
eddyiiz <eddyxsayf@gmail.com>
2024-10-08 15:51:13 +0200

    update User model

--- Doctorak.Server/Models/User.cs
+++ Doctorak.Server/Models/User.cs
@@ -11,10 +11,10 @@ public class User
     public string FirstName { get; set; } = string.Empty;
     [Required]
     public string LastName { get; set; } = string.Empty;
+    public string? VerificationToken { get; set; }
     public string? PasswordResetToken { get; set; }
+    public DateTime? ResetTokenExpires { get; set; }
     public byte[] PasswordHash { get; set; } = new byte[32];
     public byte[] PasswordSalt { get; set; } = new byte[32];
-    public string? VerificationToken { get; set; }
-    public DateTime? ResetTokenExpires { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
