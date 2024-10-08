4f648f8a1098940e731dbc3e3bd4965e47f85d54
eddyiiz <eddyxsayf@gmail.com>
2024-10-08 15:59:00 +0200

    add validPassword

--- Doctorak.Server/Services/AuthService/AuthService.cs
+++ Doctorak.Server/Services/AuthService/AuthService.cs
@@ -8,11 +8,43 @@ public class AuthService : IAuthService
     {
         _context = context;
     }
-    public Task<ServiceResponse<int>> Register(User user, string password)
+    public async Task<ServiceResponse<int>> Register(User user, string password)
     {
-        throw new NotImplementedException();
+        var response = new ServiceResponse<int>();
+
+        try
+        {
+            if (await UserExists(user.Email))
+            {
+                response.Success = false;
+                response.Message = "Email is already in use";
+
+                return response;
+            }
+            else if (!ValidPassword(password))
+            {
+                response.Success = false;
+                response.Message = "Invalid password";
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
     }
 
+    //validate password
+    private bool ValidPassword(string password)
+    {
+        return password.Length > 5;
+    }
+
+    //check user exists
     private async Task<bool> UserExists(string email)
     {
         return await _context.Users.AnyAsync(
