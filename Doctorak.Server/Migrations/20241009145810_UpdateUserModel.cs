933826496a2028f947539fbdc2a9d95850063509
eddyiiz <eddyxsayf@gmail.com>
2024-10-09 16:59:00 +0200

    update user model migration

---
+++ Doctorak.Server/Migrations/20241009145810_UpdateUserModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Doctorak.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordResetToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ResetTokenExpires",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationToken",
+                table: "Users");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Verified",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Verified",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordResetToken",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ResetTokenExpires",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VerificationToken",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
