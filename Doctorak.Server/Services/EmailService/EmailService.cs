dd5382948eb7ffee2b0b08b0a2ac5fd4d3a75c9b
eddyiiz <eddyxsayf@gmail.com>
2024-10-09 06:52:46 +0200

    add send email

---
+++ Doctorak.Server/Services/EmailService/EmailService.cs
@@ -0,0 +1,22 @@
+﻿namespace Doctorak.Server.Services.EmailService;
+
+public class EmailService : IEmailService
+{
+    private readonly IConfiguration _config;
+
+    public EmailService(IConfiguration config)
+    {
+        _config = config;
+    }
+    public async Task SendEmail(string toEmail, string subject, string body)
+    {
+        try
+        {
+            var smptSettings = _config.GetSection("SmptSettings");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
