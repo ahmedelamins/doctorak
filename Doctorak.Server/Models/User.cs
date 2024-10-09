2afd1b0464bdafc96625e4443de3005804d04244
eddyiiz <eddyxsayf@gmail.com>
2024-10-09 06:52:10 +0200

    update model

--- Doctorak.Server/Models/User.cs
+++ Doctorak.Server/Models/User.cs
@@ -5,12 +5,12 @@ public class User
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    [Required, EmailAddress]
-    public string Email { get; set; } = string.Empty;
     [Required]
     public string FirstName { get; set; } = string.Empty;
     [Required]
     public string LastName { get; set; } = string.Empty;
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
     public string? VerificationToken { get; set; }
     public string? PasswordResetToken { get; set; }
     public DateTime? ResetTokenExpires { get; set; }
