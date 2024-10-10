aac1f9921a3bb6956122610443c9d4b4b46f018d
eddyiiz <eddyxsayf@gmail.com>
2024-10-10 11:25:43 +0200

    Change password dto

---
+++ Doctorak.Server/DTOs/ChangePassword.cs
@@ -0,0 +1,7 @@
+﻿namespace Doctorak.Server.DTOs;
+public class ChangePassword
+{
+    public string password { get; set; } = string.Empty;
+    [Compare("Password")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
