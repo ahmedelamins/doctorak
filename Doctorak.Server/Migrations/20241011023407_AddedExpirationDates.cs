6429c029aabfb5fc2a2c12d65e36453d48c8614f
eddyiiz <eddyxsayf@gmail.com>
2024-10-11 04:40:15 +0200

    User: expiration dates

---
+++ Doctorak.Server/Migrations/20241011023407_AddedExpirationDates.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Doctorak.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedExpirationDates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PasswordResetCodeExipration",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VerificationCodeExpiration",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordResetCodeExipration",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationCodeExpiration",
+                table: "Users");
+        }
+    }
+}
