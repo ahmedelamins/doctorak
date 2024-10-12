03cafe00598032f3cf00071da461c7a1dec70f8f
eddyiiz <eddyxsayf@gmail.com>
2024-10-12 05:38:27 +0200

    user: add RefreshTokens

---
+++ Doctorak.Server/Migrations/20241012033652_AddRefreshTokensUser.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Doctorak.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokensUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordResetCode",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordResetCodeExipration",
+                table: "Users",
+                newName: "RefreshTokenExpiry");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "RefreshTokenExpiry",
+                table: "Users",
+                newName: "PasswordResetCodeExipration");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordResetCode",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
