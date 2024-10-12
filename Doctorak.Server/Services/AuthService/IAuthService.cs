50429888064cfc6d098f7fff8b46cd52c87ba047
eddyiiz <eddyxsayf@gmail.com>
2024-10-12 06:09:33 +0200

    uodate Login

--- Doctorak.Server/Services/AuthService/IAuthService.cs
+++ Doctorak.Server/Services/AuthService/IAuthService.cs
@@ -2,8 +2,8 @@
 public interface IAuthService
 {
     Task<ServiceResponse<int>> Register(User user, string password);
-    Task<ServiceResponse<string>> Login(string email, string password);
-    Task<ServiceResponse<string>> CreateRefreshToken(User user);
+    Task<ServiceResponse<AuthResponse>> Login(string email, string password);
+    Task<ServiceResponse<string>> RefreshAccessToken(string refreshToken);
     Task<ServiceResponse<string>> VerifyEmail(string email, string code);
     Task<ServiceResponse<string>> ForgotPassword(string email);
     Task<ServiceResponse<string>> VerifyPasswordReset(string email, string code);
