534239bc5c55e49fa96eb620023cdf6fdafa4dd8
eddyiiz <eddyxsayf@gmail.com>
2024-10-12 06:34:14 +0200

    add refresh token route

--- Doctorak.Server/Controllers/AuthController.cs
+++ Doctorak.Server/Controllers/AuthController.cs
@@ -1,5 +1,4 @@
-﻿using Doctorak.Server.DTOs;
-using Doctorak.Server.Services.AuthService;
+﻿using Doctorak.Server.Services.AuthService;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
@@ -66,6 +65,19 @@ public class AuthController : ControllerBase
         if (!response.Success)
         {
             return BadRequest(response);
+        }
+
+        return Ok(response);
+    }
+
+    [HttpPost("refresh-token")]
+    public async Task<ActionResult<ServiceResponse<string>>> RefreshToken([FromBody] RefreshToken request)
+    {
+        var response = await _authService.RefreshAccessToken(request.Token);
+
+        if (!response.Success)
+        {
+            return BadRequest(response.Message);
         }
 
         return Ok(response);
