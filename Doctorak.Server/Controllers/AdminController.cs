08b23f3fc7777a14b523e1a2a190aa9e0bf4ca15
eddyiiz <eddyxsayf@gmail.com>
2024-10-13 08:01:12 +0200

    register admin route

--- Doctorak.Server/Controllers/AdminController.cs
+++ Doctorak.Server/Controllers/AdminController.cs
@@ -1,16 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Doctorak.Server.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AdminController : ControllerBase
-    {
-        private readonly IAdminService _adminService;
+namespace Doctorak.Server.Controllers;
 
-        public AdminController(IAdminService adminService)
+[Route("api/[controller]")]
+[ApiController]
+public class AdminController : ControllerBase
+{
+    private readonly IAdminService _adminService;
+
+    public AdminController(IAdminService adminService)
+    {
+        _adminService = adminService;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult> Register([FromBody] RegisterAdmin request)
+    {
+        var response = await _adminService
+            .AdminRegister(
+                new Admin
+                {
+                    Username = request.Username
+                },
+                request.Password
+            );
+
+        if (!response.Success)
         {
-            _adminService = adminService;
+            return BadRequest(response.Message);
         }
+
+        return Ok(response);
     }
 }
