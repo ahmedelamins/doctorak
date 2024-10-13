bd9160feceb7c24afba2eca110ceceb641e705f2
eddyiiz <eddyxsayf@gmail.com>
2024-10-13 07:54:33 +0200

    inject service

---
+++ Doctorak.Server/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Doctorak.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminService _adminService;
+
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+    }
+}
