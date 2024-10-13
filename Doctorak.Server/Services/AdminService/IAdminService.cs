f48aaad0dc7b738b3843fb5a4c6436e0a424b35e
eddyiiz <eddyxsayf@gmail.com>
2024-10-13 07:22:16 +0200

    add interface

--- Doctorak.Server/Services/AdminService/IAdminService.cs
+++ Doctorak.Server/Services/AdminService/IAdminService.cs
@@ -2,4 +2,7 @@
 
 public interface IAdminService
 {
+    Task<ServiceResponse<int>> AdminReginster(User user, string password);
+    Task<ServiceResponse<string>> AdminLogin(string name, string password);
+    Task<ServiceResponse<bool>> AdminDelete(int userId);
 }
