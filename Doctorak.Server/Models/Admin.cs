5c838efefba4139d310c9e145309a10dfae978e3
eddyiiz <eddyxsayf@gmail.com>
2024-10-13 06:59:00 +0200

    add admin model

---
+++ Doctorak.Server/Models/Admin.cs
@@ -0,0 +1,12 @@
+﻿namespace Doctorak.Server.Models;
+
+public class Admin
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required]
+    public string Username { get; set; } = string.Empty;
+    public byte[] PasswordHash { get; set; } = new byte[32];
+    public byte[] PasswordSalt { get; set; } = new byte[32];
+}
