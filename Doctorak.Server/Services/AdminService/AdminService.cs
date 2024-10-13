ec7330f4e0a9a71be7cb9d4c264f67413e90efc5
eddyiiz <eddyxsayf@gmail.com>
2024-10-13 07:48:23 +0200

    create password hash

--- Doctorak.Server/Services/AdminService/AdminService.cs
+++ Doctorak.Server/Services/AdminService/AdminService.cs
@@ -23,7 +23,7 @@ public class AdminService : IAdminService
                 return response;
             }
 
-
+            GeneratePassword
 
             return response;
         }
@@ -49,7 +49,14 @@ public class AdminService : IAdminService
         return await _context.Admins.AnyAsync(a =>
             a.Username.ToLower().Equals(username.ToLower()));
     }
-
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+    }
     private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
     {
         using (var hmac = new HMACSHA512(passwordSalt))
