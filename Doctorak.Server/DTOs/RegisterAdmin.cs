4d765c2e66b939c1a45f21f462f4aaf83e9312bf
eddyiiz <eddyxsayf@gmail.com>
2024-10-13 16:55:00 +0200

    resolve

---
+++ Doctorak.Server/DTOs/RegisterAdmin.cs
@@ -0,0 +1,7 @@
+﻿namespace Doctorak.Server.DTOs;
+
+public class RegisterAdmin
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
---
+++ Doctorak.Server/Migrations/20241013091238_AddRolesToUser.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Doctorak.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRolesToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Verified",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Users",
+                newName: "JoinedAt");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "JoinedAt",
+                table: "Users",
+                newName: "CreatedAt");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Verified",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
