6292ea3f0bcecf328ac0eedce2f6653555733b9a
eddyiiz <eddyxsayf@gmail.com>
2024-10-13 07:23:42 +0200

    injecting data context

--- Doctorak.Server/Services/AdminService/AdminService.cs
+++ Doctorak.Server/Services/AdminService/AdminService.cs
@@ -1,6 +1,25 @@
-﻿
-namespace Doctorak.Server.Services.AdminServicel;
+﻿namespace Doctorak.Server.Services.AdminServicel;
 
 public class AdminService : IAdminService
 {
+    private readonly DataContext _context;
+
+    public AdminService(DataContext context)
+    {
+        _context = context;
+    }
+
+    public Task<ServiceResponse<int>> AdminReginster(User user, string password)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<ServiceResponse<string>> AdminLogin(string name, string password)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<ServiceResponse<bool>> AdminDelete(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
 }
