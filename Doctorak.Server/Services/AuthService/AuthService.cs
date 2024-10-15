99126379a4c10476d34ac6e7f55dd421320b7f30
eddyiiz <eddyxsayf@gmail.com>
2024-10-15 09:32:35 +0200

    CreateToken: claims email

--- Doctorak.Server/Services/AuthService/AuthService.cs
+++ Doctorak.Server/Services/AuthService/AuthService.cs
@@ -425,7 +425,8 @@ public class AuthService : IAuthService
         var claims = new List<Claim>
         {
             new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-            new Claim(ClaimTypes.Name, user.Email),
+            new Claim(ClaimTypes.Name, user.FirstName),
+            new Claim(ClaimTypes.Email, user.Email),
             new Claim(ClaimTypes.Role, user.Role)
         };
 
