b3bd6a410748512d5f535d92112158233a57bb8b
eddyiiz <eddyxsayf@gmail.com>
2024-10-15 09:44:21 +0200

    add doctor model

---
+++ Doctorak.Server/Models/Doctor.cs
@@ -0,0 +1,20 @@
+﻿namespace Doctorak.Server.Models;
+
+public class Doctor : User
+{
+    [Required]
+    public string Gender { get; set; } = string.Empty;
+    [Required]
+    public string Specialties { get; set; } = string.Empty;
+    [Required]
+    public string PracticeName { get; set; } = string.Empty;
+    [Required]
+    public string Location { get; set; } = string.Empty;
+    [Required]
+    public string Address { get; set; } = string.Empty;
+    [Required]
+    public string Qualification { get; set; } = string.Empty;
+    [Required]
+    public string About { get; set; } = string.Empty;
+
+}
