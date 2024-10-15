6955bb9309f70ff5c2e0f09acc15c859a4424eee
eddyiiz <eddyxsayf@gmail.com>
2024-10-15 17:08:41 +0200

    interceptors

--- doctorak.client/src/Utils/axiosInstance.js
+++ doctorak.client/src/Utils/axiosInstance.js
@@ -3,3 +3,14 @@ import axios from "axios";
 const axiosInstance = axios.Create({
     baseUrl: "https://locahost:7244/api",
 });
+
+//request intercptor injecting token into header
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("Token");  //fetch token
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
