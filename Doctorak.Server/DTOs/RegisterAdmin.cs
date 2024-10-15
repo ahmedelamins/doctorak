2cfcbe45eb95847c3b074ecf4c5c462b971ac9a6
eddyiiz <eddyxsayf@gmail.com>
2024-10-15 10:14:26 +0200

    remove unnecessary files

--- Doctorak.Server/DTOs/RegisterAdmin.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Doctorak.Server.DTOs;
-
-public class RegisterAdmin
-{
-    public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
