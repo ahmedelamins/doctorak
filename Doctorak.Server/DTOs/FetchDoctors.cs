6b0f05eab07576054f0adf124d9e7b64a4877de2
eddyiiz <eddyxsayf@gmail.com>
2024-10-15 10:41:15 +0200

    fetch users

---
+++ Doctorak.Server/DTOs/FetchDoctors.cs
@@ -0,0 +1,16 @@
+﻿namespace Doctorak.Server.DTOs;
+
+public class FetchDoctors
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Gender { get; set; } = string.Empty;
+    public string Specialties { get; set; } = string.Empty;
+    public string PracticeName { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public List<string> Qualifications { get; set; } = new List<string>();
+    public string About { get; set; } = string.Empty;
+}
