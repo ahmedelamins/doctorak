b17fac07ba313da868ab6eeacef53668021cf2bb
eddyiiz <eddyxsayf@gmail.com>
2024-10-15 10:03:07 +0200

    dr register dto

---
+++ Doctorak.Server/DTOs/DoctorRegister.cs
@@ -0,0 +1,19 @@
+﻿namespace Doctorak.Server.DTOs;
+
+public class DoctorRegister : UserRegister
+{
+    [Required]
+    public string Gender { get; set; } = string.Empty;
+    [Required]
+    public string Specialties { get; set; } = string.Empty;
+    [Required]
+    public string PracticeName { get; set; } = string.Empty;
+    [Required]
+    public string Location { get; set; } = string.Empty;
+    [Required]
+    public string Address { get; set; } = string.Empty;
+    [Required]
+    public string Qualification { get; set; } = string.Empty;
+    [Required]
+    public string About { get; set; } = string.Empty;
+}
