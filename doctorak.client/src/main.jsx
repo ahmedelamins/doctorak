51c434f0a4ebb82860e4a0c26c85c81536beed76
eddyiiz <eddyxsayf@gmail.com>
2024-10-15 16:59:41 +0200

    add theme

--- doctorak.client/src/main.jsx
+++ doctorak.client/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import Theme from './Theme';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <ThemeProvider Theme={Theme}>
+            <App />
+        </ThemeProvider>
+    </StrictMode>,
 )
