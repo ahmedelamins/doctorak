bf0f65303444730fde485e29f4a650a899f68914
eddyiiz <eddyxsayf@gmail.com>
2024-10-15 09:49:39 +0200

    add doctors table

--- Doctorak.Server/Data/DataContext.cs
+++ Doctorak.Server/Data/DataContext.cs
@@ -3,4 +3,11 @@
 public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().ToTable("users");
+        modelBuilder.Entity<Doctor>().ToTable("Doctors");  //separate table
+    }
 }
