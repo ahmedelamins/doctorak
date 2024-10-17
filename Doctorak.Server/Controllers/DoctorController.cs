230de80105cbcb3f106242b1ab76b6cbdeb16247
eddyiiz <eddyxsayf@gmail.com>
2024-10-17 14:22:09 +0200

    add slots route

---
+++ Doctorak.Server/Controllers/DoctorController.cs
@@ -0,0 +1,31 @@
+﻿using Doctorak.Server.Services.DoctorService;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Doctorak.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DoctorController : ControllerBase
+{
+
+    private readonly IDoctorService _doctorService;
+
+    public DoctorController(IDoctorService doctorService)
+    {
+        _doctorService = doctorService;
+    }
+
+    [HttpGet("fetch-slots")]
+    public async Task<ActionResult> FetchSlots()
+    {
+        var doctorId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var response = await _doctorService.FetchAvailabilitySlots(doctorId);
+
+        return response.Success ? Ok(response) : BadRequest(response);
+    }
+
+    //public async Task<ActionResult>
+    //public async Task<ActionResult>
+    //public async Task<ActionResult>
+}
