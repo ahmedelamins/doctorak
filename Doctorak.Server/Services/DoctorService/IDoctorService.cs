1e57450b5a3f43399e9d7d2ac8bdf0c066198241
eddyiiz <eddyxsayf@gmail.com>
2024-10-17 13:52:23 +0200

    update interface

--- Doctorak.Server/Services/DoctorService/IDoctorService.cs
+++ Doctorak.Server/Services/DoctorService/IDoctorService.cs
@@ -2,8 +2,8 @@
 
 public interface IDoctorService
 {
-    Task<ServiceResponse<List<AvailabilitySlot>>> GetAvailabilitySlots(int doctorId);
-    Task<ServiceResponse<AvailabilitySlot>> GetAvailabilitySlot(int doctorId, int slotId);
+    Task<ServiceResponse<List<AvailabilitySlot>>> FetchAvailabilitySlots(int doctorId);
+    Task<ServiceResponse<AvailabilitySlot>> FetchAvailabilitySlot(int doctorId, int slotId);
     Task<ServiceResponse<AvailabilitySlot>> CreateAvailabilitySlot(int doctorId, AvailabilitySlot slot);
     Task<ServiceResponse<AvailabilitySlot>> UpdateAvailabilitySlot(int doctorId, int slotId, AvailabilitySlot updatedSlot);
     Task<ServiceResponse<bool>> DeleteAvailabilitySlot(int doctorId, int slotId);
