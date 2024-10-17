ae4b8c09aa4bb1bf2cba4f291cae94f441a6d2c8
eddyiiz <eddyxsayf@gmail.com>
2024-10-17 17:00:39 +0200

    debugging

---
+++ Doctorak.Server/Migrations/20241017143424_RestBugTable.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Doctorak.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestBugTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AvailabilitySlots_Doctors_DoctorId",
+                table: "AvailabilitySlots");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AvailabilitySlots",
+                table: "AvailabilitySlots");
+
+            migrationBuilder.RenameTable(
+                name: "AvailabilitySlots",
+                newName: "AvailabilitySlot");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AvailabilitySlots_DoctorId",
+                table: "AvailabilitySlot",
+                newName: "IX_AvailabilitySlot_DoctorId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AvailabilitySlot",
+                table: "AvailabilitySlot",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AvailabilitySlot_Doctors_DoctorId",
+                table: "AvailabilitySlot",
+                column: "DoctorId",
+                principalTable: "Doctors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AvailabilitySlot_Doctors_DoctorId",
+                table: "AvailabilitySlot");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AvailabilitySlot",
+                table: "AvailabilitySlot");
+
+            migrationBuilder.RenameTable(
+                name: "AvailabilitySlot",
+                newName: "AvailabilitySlots");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AvailabilitySlot_DoctorId",
+                table: "AvailabilitySlots",
+                newName: "IX_AvailabilitySlots_DoctorId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AvailabilitySlots",
+                table: "AvailabilitySlots",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AvailabilitySlots_Doctors_DoctorId",
+                table: "AvailabilitySlots",
+                column: "DoctorId",
+                principalTable: "Doctors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Doctorak.Server/Migrations/20241017145906_DoctorIDCreate.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Doctorak.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class DoctorIDCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AvailabilitySlots_Doctors_UserId",
+                table: "AvailabilitySlots");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "AvailabilitySlots",
+                newName: "DoctorId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AvailabilitySlots_UserId",
+                table: "AvailabilitySlots",
+                newName: "IX_AvailabilitySlots_DoctorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AvailabilitySlots_Doctors_DoctorId",
+                table: "AvailabilitySlots",
+                column: "DoctorId",
+                principalTable: "Doctors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AvailabilitySlots_Doctors_DoctorId",
+                table: "AvailabilitySlots");
+
+            migrationBuilder.RenameColumn(
+                name: "DoctorId",
+                table: "AvailabilitySlots",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AvailabilitySlots_DoctorId",
+                table: "AvailabilitySlots",
+                newName: "IX_AvailabilitySlots_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AvailabilitySlots_Doctors_UserId",
+                table: "AvailabilitySlots",
+                column: "UserId",
+                principalTable: "Doctors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
