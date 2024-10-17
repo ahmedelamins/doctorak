168cd9001eeed945ddb014e65e14dc1258576997
eddyiiz <eddyxsayf@gmail.com>
2024-10-17 13:44:56 +0200

    add interface

---
+++ Doctorak.Server/Services/DoctorService/IDoctorService.cs
@@ -0,0 +1,10 @@
+﻿namespace Doctorak.Server.Services.DoctorService;
+
+public interface IDoctorService
+{
+    Task<ServiceResponse<List<AvailabilitySlot>>> GetAvailabilitySlots(int doctorId);
+    Task<ServiceResponse<AvailabilitySlot>> GetAvailabilitySlot(int doctorId, int slotId);
+    Task<ServiceResponse<AvailabilitySlot>> CreateAvailabilitySlot(int doctorId, AvailabilitySlot slot);
+    Task<ServiceResponse<AvailabilitySlot>> UpdateAvailabilitySlot(int doctorId, int slotId, AvailabilitySlot updatedSlot);
+    Task<ServiceResponse<bool>> DeleteAvailabilitySlot(int doctorId, int slotId);
+}
