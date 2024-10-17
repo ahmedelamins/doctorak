27aaa19e6c08130ba0d388dee51a508a8afb8d89
eddyiiz <eddyxsayf@gmail.com>
2024-10-17 13:47:00 +0200

    add doctor service

---
+++ Doctorak.Server/Services/DoctorService/DoctorService.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Doctorak.Server.Services.DoctorService;
+
+public class DoctorService : IDoctorService
+{
+    public Task<ServiceResponse<List<AvailabilitySlot>>> GetAvailabilitySlots(int doctorId)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<ServiceResponse<AvailabilitySlot>> GetAvailabilitySlot(int doctorId, int slotId)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<ServiceResponse<AvailabilitySlot>> CreateAvailabilitySlot(int doctorId, AvailabilitySlot slot)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse<AvailabilitySlot>> UpdateAvailabilitySlot(int doctorId, int slotId, AvailabilitySlot updatedSlot)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse<bool>> DeleteAvailabilitySlot(int doctorId, int slotId)
+    {
+        throw new NotImplementedException();
+    }
+
+}
