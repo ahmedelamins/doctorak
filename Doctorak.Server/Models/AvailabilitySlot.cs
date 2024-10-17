e183a5135751402bc671889f591982ea8e382912
eddyiiz <eddyxsayf@gmail.com>
2024-10-17 13:24:13 +0200

    add available slot model

---
+++ Doctorak.Server/Models/AvailabilitySlot.cs
@@ -0,0 +1,12 @@
+﻿namespace Doctorak.Server.Models;
+
+public class AvailabilitySlot
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public int DoctorId { get; set; }
+    public DayOfWeek Day { get; set; }
+    public TimeSpan Starts { get; set; }
+    public TimeSpan Ends { get; set; }
+}
