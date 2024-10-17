6db2631f1f0f1ddc32b68dc07a176fac86b124a4
eddyiiz <eddyxsayf@gmail.com>
2024-10-17 09:37:50 +0200

    add appointment model

---
+++ Doctorak.Server/Models/Appointment.cs
@@ -0,0 +1,15 @@
+﻿namespace Doctorak.Server.Models;
+
+public class Appointment
+{
+    public int Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public int UserId { get; set; }
+    public int DoctorId { get; set; }
+    public DateOnly Date { get; set; }
+    public TimeOnly Time { get; set; }
+    public string Status { get; set; } = "Pending";
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set; }
+    public string? CancellationReason { get; set; }
+}
