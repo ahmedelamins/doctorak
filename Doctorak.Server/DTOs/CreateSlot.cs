174efda27fe1641da5cafed06e0e72b02eb4f095
eddyiiz <eddyxsayf@gmail.com>
2024-10-17 15:58:26 +0200

    add create slot

---
+++ Doctorak.Server/DTOs/CreateSlot.cs
@@ -0,0 +1,12 @@
+﻿namespace Doctorak.Server.DTOs;
+
+public class CreateSlot
+{
+    public int DoctorId { get; set; }
+    [Required]
+    public string Day { get; set; } = string.Empty;
+    [Required]
+    public TimeOnly Starts { get; set; }
+    [Required]
+    public TimeOnly Ends { get; set; }
+}
