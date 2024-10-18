e338c2349403457ffe1c45f42506cce6baa4f37d
eddyiiz <eddyxsayf@gmail.com>
2024-10-18 04:42:29 +0200

    update database migration

---
+++ Doctorak.Server/Migrations/20241018024146_UpdateDB.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Doctorak.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Breaks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AvailabilitySlotId = table.Column<int>(type: "int", nullable: false),
+                    BreakStarts = table.Column<TimeOnly>(type: "time", nullable: false),
+                    BreakEnds = table.Column<TimeOnly>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Breaks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Breaks_AvailabilitySlots_AvailabilitySlotId",
+                        column: x => x.AvailabilitySlotId,
+                        principalTable: "AvailabilitySlots",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Breaks_AvailabilitySlotId",
+                table: "Breaks",
+                column: "AvailabilitySlotId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Breaks");
+        }
+    }
+}
