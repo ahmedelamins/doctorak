7f38aeb3f7a305cabba9b2c2018bc8ee417be3ee
eddyiiz <eddyxsayf@gmail.com>
2024-10-18 03:30:00 +0200

    add DTO

---
+++ Doctorak.Server/DTOs/UpdateSlot.cs
@@ -0,0 +1,8 @@
+﻿namespace Doctorak.Server.DTOs;
+
+public class UpdateSlot
+{
+    public string Day { get; set; } = string.Empty;
+    public TimeOnly Starts { get; set; }
+    public TimeOnly Ends { get; set; }
+}
