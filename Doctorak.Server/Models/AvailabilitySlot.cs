2b8802f42b797ccd34e98f9ed16da2dfd8832a10
eddyiiz <eddyxsayf@gmail.com>
2024-10-18 04:23:22 +0200

    update slots with breaks list

--- Doctorak.Server/Models/AvailabilitySlot.cs
+++ Doctorak.Server/Models/AvailabilitySlot.cs
@@ -12,4 +12,5 @@ public class AvailabilitySlot
     public TimeOnly Starts { get; set; }
     [Required]
     public TimeOnly Ends { get; set; }
+    public List<BreakSlot> Breaks { get; set; } = new List<BreakSlot>();
 }
