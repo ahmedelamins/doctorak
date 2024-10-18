c934b666189e6a25619748461d9035e327f4fa1c
eddyiiz <eddyxsayf@gmail.com>
2024-10-18 04:21:42 +0200

    add break slot model

---
+++ Doctorak.Server/Models/BreakSlot.cs
@@ -0,0 +1,12 @@
+﻿namespace Doctorak.Server.Models;
+
+public class BreakSlot
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public int AvailabilitySlotId { get; set; }
+    public TimeOnly BreakStarts { get; set; }
+
+    public TimeOnly BreakEnds { get; set; }
+}
